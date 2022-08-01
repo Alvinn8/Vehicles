2fe765b05a31eb768122086a7e3c0a72f59cb07c
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2022-08-01 19:29:47 +0200

    Slight changes to motorcycle health and size

--- Vehicles/src/main/java/me/alvin/vehicles/vehicle/VehicleTypes.java
+++ Vehicles/src/main/java/me/alvin/vehicles/vehicle/VehicleTypes.java
@@ -108,7 +108,7 @@ public class VehicleTypes {
                 new ThirdPersonPerspective(new RelativePos(0, 0, -5))
             ),
             // Max Health
-            100,
+            60,
             // Enableable
             SVCraftVehicles.getInstance(),
             // Recipe
@@ -305,7 +305,7 @@ public class VehicleTypes {
             MotorcycleVehicle::new,
             MotorcycleVehicle::new,
             // Collision
-            new AABBCollision(1, 1.5),
+            new AABBCollision(0.75, 1.5),
             // Seat
             new Seat(new RelativePos(-0.1, 1.3, -0.8)),
             null,
